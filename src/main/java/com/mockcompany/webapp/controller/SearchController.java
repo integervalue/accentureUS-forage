a142499c6916dfb9fbfffc878c9a63e243235dbd
Andrei Vasiliu <106980546+integervalue@users.noreply.github.com>
2024-08-14 21:44:53 +0100

    Add own code for tasks 1 and 2.
    Added own implementations to tasks 1 and 2.

--- src/main/java/com/mockcompany/webapp/controller/SearchController.java
+++ src/main/java/com/mockcompany/webapp/controller/SearchController.java
@@ -14,16 +14,19 @@ package com.mockcompany.webapp.controller;
 /*
  * An import statement allows the current class to use the class being imported
  */
+import com.mockcompany.webapp.data.ProductItemRepository;
 import com.mockcompany.webapp.model.ProductItem;
-import com.mockcompany.webapp.service.SearchService;
 /* The springframework package allows us to take advantage of the spring capabilities */
+import com.mockcompany.webapp.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /* java.util package provides useful utilities */
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * This class is the entrypoint for the /api/products/search API.  It is "annotated" with
@@ -40,14 +43,13 @@ import java.util.Collection;
 @RestController
 public class SearchController {
 
-    // Inject the new service into our class
-    private final SearchService searchService;
-
-    // Update constructor to inject SearchService
+    /**
+     * This is a instance field.  It is provided by the spring framework through the constructor because of the
+     * @Autowired annotation.  Autowire tells the spring framework to automatically find and use an instance of
+     * the declared class when creating this class.
+     */
     @Autowired
-    public SearchController(SearchService searchService) {
-        this.searchService = searchService;
-    }
+    SearchService searchService;
 
     /**
      * The search method, annotated with @GetMapping telling spring this method should be called
@@ -59,7 +61,22 @@ public class SearchController {
      */
     @GetMapping("/api/products/search")
     public Collection<ProductItem> search(@RequestParam("query") String query) {
-        // Implementation of method was moved to service, delegate query to the service now
-        return this.searchService.search(query);
+        /*
+         * TODO: !!!! Implement this method !!!!
+         *  The easiest implementation will be to use the findAll as we are below. Then filter using Java
+         *  string methods such as contains(...), toLowerCase(...), equals(...), etc.
+         *
+         *  The requirements are defined in src/test/groovy/com/mockcompany/webapp/controller/SearchControllerSpec.groovy
+         *
+         *  Read through the tests to get an idea of how search should work.  When the tests are written before the code,
+         *  it is known as Test Driven Development (TDD) and is a common best practice. The Spock framework is a great
+         *  framework for TDD because the tests are written very descriptively using sentences.
+         *
+         *    https://spockframework.org/spock/docs/2.0/spock_primer.html
+         *
+         *  For an added challenge, update the ProductItemRepository to do the filtering at the database layer :)
+         */
+
+        return searchService.search(query);
     }
 }
--- src/main/java/com/mockcompany/webapp/service/SearchService.java
+++ src/main/java/com/mockcompany/webapp/service/SearchService.java
@@ -4,6 +4,8 @@ import com.mockcompany.webapp.data.ProductItemRepository;
 import com.mockcompany.webapp.model.ProductItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -11,7 +13,11 @@ import java.util.List;
 
 @Service
 public class SearchService {
-
+    /**
+     * This is a instance field.  It is provided by the spring framework through the constructor because of the
+     * @Autowired annotation.  Autowire tells the spring framework to automatically find and use an instance of
+     * the declared class when creating this class.
+     */
     private final ProductItemRepository productItemRepository;
 
     @Autowired
@@ -20,55 +26,33 @@ public class SearchService {
     }
 
     public Collection<ProductItem> search(String query) {
-        /*
-         * This is a simple implementation that loops over all the items and does the filtering in Java.
-         * A better implementation would do most if not all of the filtering in a query and then finalize or
-         * rank the results in Java.  From the SearchControllerSpec, we need to meet the following requirements:
-         *   1. query can be contained within either the name or description of the item
-         *   2. query string is treated as case-insensitive meaning Hi will match hi, hI, Hi, or HI
-         *   3. If the query is wrapped in quotes, only EXACT matches of name/description will be returned
-         */
+
         Iterable<ProductItem> allItems = this.productItemRepository.findAll();
         List<ProductItem> itemList = new ArrayList<>();
 
-        /*
-         * 1. Check for quotes in the query string. We should use a regex for this but for simplicity
-         *    we will just check and extract using startsWith/endsWith/subString
-         */
-        boolean exactMatch = false;
-        if (query.startsWith("\"") && query.endsWith("\"")) {
-            exactMatch = true;
-            // Extract the quotes
-            query = query.substring(1, query.length() - 1);
-        } else {
-            // Handle case-insensitivity by converting to lowercase first
-            query = query.toLowerCase();
-        }
+        // Determine whether the user wants exact matches to their search
+        boolean isExactMatch = (query.startsWith("\"") && query.endsWith("\"")) || (query.startsWith("\'") && query.endsWith("\'"));
 
-        // For each item... This is written for simplicity to be read/understood not necessarily maintain or extend
+        // If yes to exact matches, truncate the extra quotes
+        String lowerCaseQuery = isExactMatch
+                ? query.substring(1, query.length() - 1).toLowerCase()
+                : query.toLowerCase();
+
         for (ProductItem item : allItems) {
-            boolean nameMatches;
-            boolean descMatches;
-            // Check if we are doing exact match or not
-            if (exactMatch) {
-                // Check if name is an exact match
-                nameMatches = query.equals(item.getName());
-                // Check if description is an exact match
-                descMatches = query.equals(item.getDescription());
+            if (isExactMatch) {
+                // Check for matches in both item name and description
+                if ((item.getName() != null && item.getName().equalsIgnoreCase(lowerCaseQuery)) ||
+                        (item.getDescription() != null && item.getDescription().equalsIgnoreCase(lowerCaseQuery))) {
+                    itemList.add(item);
+                }
             } else {
-                // We are doing a contains ignoring case check, normalize everything to lowercase
-                // Check if name contains query
-                nameMatches = item.getName().toLowerCase().contains(query);
-                // Check if description contains query
-                descMatches = item.getDescription().toLowerCase().contains(query);
-            }
-
-            // If either one matches, add to our list
-            if (nameMatches || descMatches) {
-                itemList.add(item);
+                // Check for matches in both item name and description
+                if ((item.getName() != null && item.getName().toLowerCase().contains(lowerCaseQuery)) ||
+                        (item.getDescription() != null && item.getDescription().toLowerCase().contains(lowerCaseQuery))) {
+                    itemList.add(item);
+                }
             }
         }
-        // Return results
         return itemList;
     }
 }
